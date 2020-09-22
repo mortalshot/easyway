d7815f5dc7daf97ce57d8eb0c33db2fc6df7ab0c
mortalshot <seregagl1996@gmail.com>
2020-09-22 17:47:36 +0300

    help

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function () {
     $('#sideCategories-accordion .accordion__title:first').click();
+
+    $('.help-tabs__item').click(function () {
+        $('.popup').removeClass('open');
+        $('body').removeClass('lock');
+        $('body').css({ paddingRight: 0 });
+        $('.help-page__menu-mobile a').removeClass('active');
+    });
+
+    $('.help-page__menu-mobile a').click(function () {
+        $('.help-page__menu-mobile a').toggleClass('active');
+    });
 }); 
--- easyway/js/script.es5.js
+++ easyway/js/script.es5.js
@@ -4006,4 +4006,15 @@ $('.banner-slider').slick({
 });
 $(document).ready(function () {
   $('#sideCategories-accordion .accordion__title:first').click();
+  $('.help-tabs__item').click(function () {
+    $('.popup').removeClass('open');
+    $('body').removeClass('lock');
+    $('body').css({
+      paddingRight: 0
+    });
+    $('.help-page__menu-mobile a').removeClass('active');
+  });
+  $('.help-page__menu-mobile a').click(function () {
+    $('.help-page__menu-mobile a').toggleClass('active');
+  });
 });
