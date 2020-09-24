b5c1320369a7076ca0e1348315244a00ee6a8667
mortalshot <seregagl1996@gmail.com>
2020-09-24 17:17:55 +0300

    lk

--- #src/js/components/lk.js
+++ #src/js/components/lk.js
@@ -5,6 +5,17 @@ $(document).ready(function () {
         $(this).siblings('input').prop('type', type);
         // $(this).toggleClass('');
     })
+    $('.lk-wrapper__mb-navigation-btn').click(function(e){
+        e.preventDefault();
+        if(window.innerWidth < 951){
+            $('.lk-wrapper__sidebar').addClass('lk-wrapper__sidebar--active');
+        }
+    });
+    $('.lk-wrapper__sidebar-title').click(function(){
+        if(window.innerWidth < 951){
+            $('.lk-wrapper__sidebar').removeClass('lk-wrapper__sidebar--active');
+        }
+    })
 
     $('.lk-wrapper__content-tab').click(function(e){
     	e.preventDefault();
@@ -28,19 +39,21 @@ $(document).ready(function () {
             $(this).addClass('lk-wrapper__content-tab--active');
 
 
-            $('.signin-wrapper__auth-box').removeClass('signin-wrapper__auth-box--active');
+            // $('.signin-wrapper__auth-box').removeClass('signin-wrapper__auth-box--active');
+            $('.lk-wrapper__content-box').removeClass('signin-wrapper__auth-box--active');
             $(href).addClass('signin-wrapper__auth-box--active');
         }
     });
 
 
-
+    // Add address
     $('.lk-wrapper__add-address').click(function(){
         var clone = $('.lk-wrapper__form-address:first').clone();
         clone.find('input').val('');
         $(this).before(clone);
     });
 
+    // remove review
     $('.lk-reviews-item__remove').click(function(e){
         e.preventDefault();
         var review = $(this).closest('.lk-reviews-item').fadeOut(400, function() { $(this).remove(); });
