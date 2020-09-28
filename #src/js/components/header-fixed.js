8d5b978ff080d40e5fa921a23b4a5ff8bc9fadd2
mortalshot <seregagl1996@gmail.com>
2020-09-28 09:38:06 +0300

    more and more fix

--- #src/js/components/header-fixed.js
+++ #src/js/components/header-fixed.js
@@ -1,10 +1,12 @@
-const navOffset = $('.site_header__bottom').offset().top;
-$(window).scroll(function () {
-    const scrolled = $(this).scrollTop();
+$(document).ready(function () {
+    const navOffset = $('.site_header__bottom').offset().top;
+    $(window).scroll(function () {
+        const scrolled = $(this).scrollTop();
 
-    if (scrolled > navOffset) {
-        $('.site_wrap').addClass('nav-fixed');
-    } else if (scrolled < navOffset) {
-        $('.site_wrap').removeClass('nav-fixed');
-    }
-});
+        if (scrolled > navOffset) {
+            $('.site_wrap').addClass('nav-fixed');
+        } else if (scrolled < navOffset) {
+            $('.site_wrap').removeClass('nav-fixed');
+        }
+    });
+})
