014895473eabd4d0bb3351a34be05629b0f74547
mortalshot <seregagl1996@gmail.com>
2020-09-28 10:01:56 +0300

    header fix

--- easyway/js/script.es5.js
+++ easyway/js/script.es5.js
@@ -5224,16 +5224,23 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 }); // components
 
 $(document).ready(function () {
-  $(".basket_list").niceScroll({
-    cursorwidth: "8px",
-    cursorborderradius: "5px",
-    cursorcolor: "#8D8F9A"
-  });
-  $(".summary-cart__products-wrapper").niceScroll({
-    cursorwidth: "8px",
-    cursorborderradius: "5px",
-    cursorcolor: "#8D8F9A"
-  });
+  mediaQueryMdMin = window.matchMedia('(min-width: 768px)');
+  mediaQueryMdMin.addListener(handleTabletChange);
+
+  function handleTabletChange(e) {
+    if (e.matches) {
+      $(".basket_list").niceScroll({
+        cursorwidth: "8px",
+        cursorborderradius: "5px",
+        cursorcolor: "#8D8F9A"
+      });
+      $(".summary-cart__products-wrapper").niceScroll({
+        cursorwidth: "8px",
+        cursorborderradius: "5px",
+        cursorcolor: "#8D8F9A"
+      });
+    }
+  }
 });
 $(document).ready(function () {
   function testWebP(callback) {
--- easyway/js/script.js
+++ easyway/js/script.js
@@ -145,16 +145,22 @@
 
 // components
 $(document).ready(function () {
-    $(".basket_list").niceScroll({
-        cursorwidth: "8px",
-        cursorborderradius: "5px",
-        cursorcolor: "#8D8F9A",
-    });
-    $(".summary-cart__products-wrapper").niceScroll({
-        cursorwidth: "8px",
-        cursorborderradius: "5px",
-        cursorcolor: "#8D8F9A",
-    });
+    mediaQueryMdMin = window.matchMedia('(min-width: 768px)');
+    mediaQueryMdMin.addListener(handleTabletChange);
+    function handleTabletChange(e) {
+        if (e.matches) {
+            $(".basket_list").niceScroll({
+                cursorwidth: "8px",
+                cursorborderradius: "5px",
+                cursorcolor: "#8D8F9A",
+            });
+            $(".summary-cart__products-wrapper").niceScroll({
+                cursorwidth: "8px",
+                cursorborderradius: "5px",
+                cursorcolor: "#8D8F9A",
+            });
+        }
+    }
 })
 $(document).ready(function () {
 	function testWebP(callback) {
